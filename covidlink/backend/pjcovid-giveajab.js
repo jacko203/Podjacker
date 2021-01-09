7704a897d289cf64739ca0ffc9c1d381845cba2c
jacko203 <rob.jacko.jackson@gmail.com>
2021-01-09 17:42:26 +0000

    sig updates - switch/case and working with frontend

--- covidlink/backend/pjcovid-giveajab.js
+++ covidlink/backend/pjcovid-giveajab.js
@@ -17,145 +17,32 @@ class response_obj {
 
 exports.handler = async (event) => {
 
-    //make this better
-    if(event.operation == "dovaccinate")
+    console.log("API invoked");
+
+    var key = event.data;
+
+    switch (key.operation)
     {
-      console.log("in do-vaccinate");
-
-      let date = new Date();
-      let now = date.toISOString();
-      var int_cubicle = event.cubicle;
-
-      let params = {
-        TableName:'text-pjcovid-vaccines',
-        Item: {
-            'cubicle': int_cubicle,
-            'timestamp': now
-        }
-      };
-
-      var vialincrement = {
-        TableName:'pjcovid-vials',
-        Key: {
-          "cubicle":int_cubicle
-        },
-        UpdateExpression: 'add thisvial :increment, thiscubicle :increment',
-        ExpressionAttributeValues: {
-          ":increment": 1
-          },
-        ReturnValues: 'ALL_NEW'
-      };
-
-    // Using await, make sure object writes to DynamoDB table before continuing execution
-    console.log("Putting item:" +int_cubicle);
-
-    //var docClient = new AWS.DynamoDB.DocumentClient();
-
-    var readparams = {
-          TableName : "text-pjcovid-vaccines",
-          KeyConditionExpression: "cubicle = :cubval",
-          ExpressionAttributeValues: {
-              ":cubval": int_cubicle
-              }
-            };
-
-    console.log("Getting item:"+int_cubicle);
-
-    try {
-
-          await dynamodb.put(params).promise();
-          console.log("put promise resolved");
-        // Now query for all from this cubicle
-          let data = await dynamodb.query(readparams).promise();
-
-          console.log("about to create return array");
-          json_return_array = JSON.stringify(data.Items);
-
-          let vials_back = await dynamodb.update(vialincrement).promise();
-          if (vials_back == null) { console.log("null return from vials");
-              }
-          else {
-            vials_return_array = JSON.stringify(vials_back);
-            console.log("Vials return"+vials_return_array);
-          }
-
-
-        } catch (err) { console.log(err) }
-
-    console.log("Returning");
-
-      return new response_obj(200, { overview: json_return_array,
-                                     vial: vials_return_array});
+      case "dovaccinate": return pjc_vaccinate(key.cubicle);
+      case "removejab": return pjc_removejab(key.cubicle, key.timestamp);
+      case "newvial": return pjc_newvial(key.cubicle, key.vialdoses);
+      case "gpoverview": return pjc_gpoverview(key.cubicle_count);
+      default: return new response_obj(400,JSON.stringify("No event operation"));
     }
 
-    else if (event.operation == 'removejab')
-    {
-      var cubicle = event.cubicle;
-      var timestamp = event.timestamp;
+};
 
-      console.log("remove jab: t:"+timestamp+"from c:"+cubicle);
 
-      // remove timestamp/dose
-      var removeparams = {
-        TableName: "text-pjcovid-vaccines",
-        Key: {  "cubicle": cubicle,
-                "timestamp": timestamp },
-        ReturnValues: "ALL_OLD"  //returns empty if nothing deleted
-      };
+async function pjc_gpoverview(cubicle_count) {
 
-      // subtract thiscubicle by 1
-      var vialdecrement = {
-        TableName:'pjcovid-vials',
-        Key: {
-          "cubicle":cubicle
-        },
-        UpdateExpression: 'add thisvial :increment, thiscubicle :increment',
-        ExpressionAttributeValues: {
-          ":increment": -1
-          },
-        ReturnValues: 'ALL_NEW'
-      };
-
-      var readparams = {
-            TableName : "text-pjcovid-vaccines",
-            KeyConditionExpression: "cubicle = :cubval",
-            ExpressionAttributeValues: {
-                ":cubval": cubicle
-                }
-              };
-
-      try {
-        let r = await dynamodb.delete(removeparams).promise();
-      // delete last item
-        console.log("delete return:"+r);
-
-        var vialdata = await dynamodb.update(vialdecrement).promise();
-          if(vialdata==null) throw new Error("null return delete");
-
-        vials_return_array = JSON.stringify(vialdata);
-
-        var data = await dynamodb.query(readparams).promise();
-        json_return_array = JSON.stringify(data.Items);
-
-    } catch (err) { console.log(err) }
-        return new response_obj(200, { overview: json_return_array,
-                                       vial: vials_return_array
-                                     });
-
-    }
-
-    else if (event.operation == 'gpoverview')
-    {
-
-      console.log("GP Lead screen");
-
-      let cubicle_count = event.cubicle_count;
+      console.log("GP Lead screen: "+cubicle_count);
+      var int_cubicle_count = parseInt(cubicle_count);
 
       //scan table then local function to return into array
       var scanparams = {
             TableName : "pjcovid-vials",
             FilterExpression : "cubicle <= :cubicle_count",
-            ExpressionAttributeValues : { ":cubicle_count": cubicle_count }
+            ExpressionAttributeValues : { ":cubicle_count": int_cubicle_count }
           };
 
 
@@ -169,12 +56,14 @@ exports.handler = async (event) => {
 
         try {
 
-            var data = await dynamodb.scan(scanparams).promise();
-            live_data.vars = JSON.stringify(data.Items);
+            var scan_data = await dynamodb.scan(scanparams).promise();
+            live_data.vars = JSON.stringify(scan_data.Items);
 
           } catch (err) {
             console.log("error:"+err);
           }
+
+          console.log("New vars:"+live_data.vars);
 
           //add 5 seconds to when will next allow fetch
           live_data.time = timer.setSeconds( timer.getSeconds()+5 );
@@ -187,36 +76,164 @@ exports.handler = async (event) => {
       console.log("Scanned items:"+live_data.vars);
 
       return new response_obj(200,live_data.vars);
-    }
+}
 
+
+/*
     else if (event.operation == 'pharmaoverview')
     {
       console.log("Pharma overview");
     }
+  } */
 
-    else if (event.operation == 'newvial')
-    {
-      var nvcubicle = event.cubicle;
-      var doses = event.vialdoses;
-      console.log("new vial for:"+nvcubicle+"with doses:"+doses);
-      // reset cubicle counter to number
 
-      var vialset = {
-        TableName:'pjcovid-vials',
-        Key: {
-          "cubicle":nvcubicle
-        },
-        UpdateExpression: 'set thisvial :base, pervial :pervial',
-        ExpressionAttributeValues: {
-          ":base": 0,
-          ":pervial": doses
-          },
-        ReturnValues: 'ALL_NEW'
-      };
 
-      let nvdata = await dynamodb.update(vialset).promise();
-      vials_return_array = JSON.stringify(nvdata);
 
-      return new response_obj(200, { vial: vials_return_array });
+async function pjc_vaccinate(cubicle) {
+
+  console.log("in do-vaccinate");
+
+  let date = new Date();
+  let now = date.toISOString();
+  var int_cubicle = parseInt(cubicle);
+
+  let params = {
+    TableName:'text-pjcovid-vaccines',
+    Item: {
+      'cubicle': int_cubicle,
+      'timestamp': now
     }
-};
+  };
+
+  var vialincrement = {
+    TableName:'pjcovid-vials',
+    Key: {
+        "cubicle":int_cubicle
+        },
+    UpdateExpression: 'add thisvial :increment, thiscubicle :increment',
+    ExpressionAttributeValues: {
+        ":increment": 1
+    },
+    ReturnValues: 'ALL_NEW'
+  };
+
+  // Using await, make sure object writes to DynamoDB table before continuing execution
+  console.log("Putting item:" +int_cubicle);
+
+  //var docClient = new AWS.DynamoDB.DocumentClient();
+
+  var readparams = {
+    TableName : "text-pjcovid-vaccines",
+    KeyConditionExpression: "cubicle = :cubval",
+    ExpressionAttributeValues: {
+      ":cubval": int_cubicle
+    }
+  };
+
+  console.log("Getting item:"+int_cubicle);
+
+  try {
+
+    await dynamodb.put(params).promise();
+    console.log("put promise resolved");
+// Now query for all from this cubicle
+    let data = await dynamodb.query(readparams).promise();
+
+    console.log("about to create return array");
+    json_return_array = JSON.stringify(data.Items);
+
+    let vials_back = await dynamodb.update(vialincrement).promise();
+    if (vials_back == null) { console.log("null return from vials");
+      }
+      else {
+    vials_return_array = JSON.stringify(vials_back);
+    console.log("Vials return"+vials_return_array);
+    }
+
+
+    } catch (err) { console.log(err) }
+
+
+    console.log("Returning");
+    return new response_obj(200, { overview: json_return_array,
+                             vial: vials_return_array});
+
+}
+
+async function pjc_removejab(cubicle, timestamp) {
+
+    console.log("remove jab: t:"+timestamp+"from c:"+cubicle);
+
+    // remove timestamp/dose
+    var removeparams = {
+      TableName: "text-pjcovid-vaccines",
+      Key: {  "cubicle": cubicle,
+              "timestamp": timestamp },
+      ReturnValues: "ALL_OLD"  //returns empty if nothing deleted
+    };
+
+    // subtract thiscubicle by 1
+    var vialdecrement = {
+      TableName:'pjcovid-vials',
+      Key: {
+        "cubicle":cubicle
+      },
+      UpdateExpression: 'add thisvial :increment, thiscubicle :increment',
+      ExpressionAttributeValues: {
+        ":increment": -1
+        },
+      ReturnValues: 'ALL_NEW'
+    };
+
+    var readparams = {
+          TableName : "text-pjcovid-vaccines",
+          KeyConditionExpression: "cubicle = :cubval",
+          ExpressionAttributeValues: {
+              ":cubval": cubicle
+              }
+            };
+
+    try {
+      let r = await dynamodb.delete(removeparams).promise();
+    // delete last item
+      console.log("delete return:"+r);
+
+      var vialdata = await dynamodb.update(vialdecrement).promise();
+        if(vialdata==null) throw new Error("null return delete");
+
+      vials_return_array = JSON.stringify(vialdata);
+
+      var data = await dynamodb.query(readparams).promise();
+      json_return_array = JSON.stringify(data.Items);
+
+  } catch (err) { console.log(err) }
+
+
+      return new response_obj(200, { overview: json_return_array,
+                                     vial: vials_return_array
+                                   });
+}
+
+async function pjc_newvial(nvcubicle, doses) {
+
+    console.log("new vial for:"+nvcubicle+"with doses:"+doses);
+    // reset cubicle counter to number
+
+    var vialset = {
+      TableName:'pjcovid-vials',
+      Key: {
+        "cubicle":nvcubicle
+      },
+      UpdateExpression: 'set thisvial :base, pervial :pervial',
+      ExpressionAttributeValues: {
+        ":base": 0,
+        ":pervial": doses
+        },
+      ReturnValues: 'ALL_NEW'
+    };
+
+    let nvdata = await dynamodb.update(vialset).promise();
+    vials_return_array = JSON.stringify(nvdata);
+
+    return new response_obj(200, { vial: vials_return_array });
+  }
